6fee6f3b65c3bf90c8d8d116c69157189be6a6b3
YashSingh03 <vyash.singh5757@gmail.com>
2025-09-06 14:37:19 +0530

    hi

--- projects/algoproject/smart_contracts/hello_world/contract.algo.ts
+++ projects/algoproject/smart_contracts/hello_world/contract.algo.ts
@@ -1,7 +1,18 @@
-import { Contract } from '@algorandfoundation/algorand-typescript'
+import { Contract, GlobalState } from '@algorandfoundation/algorand-typescript'
 
 export class HelloWorld extends Contract {
-  hello(name: string): string {
-    return `Hello, ${name}`
+  watchlist = GlobalState<string>({ key: "watchlist", initialValue: "" });
+
+  // Add a movie to the watchlist
+  AddMovie(title: string, description: string): string {
+    // Format: Title (Year): Description;
+    const entry = `${title}: ${description}; `;
+    this.watchlist.value = this.watchlist.value + entry;
+    return title;
+  }
+
+  // View the full watchlist
+  GetWatchlist(): string {
+    return this.watchlist.value;
   }
 }
